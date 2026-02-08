01421959d3623f984dd64b944ec24f61bb95984a
CrazyHayz02 <aaron.hayes02@gmail.com>
2026-02-08 00:06:14 +0000

    took out migrate on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 set -e  # Exit immediately on error
 
 # ------------------------------
-# Apply migrations
-# ------------------------------
-echo "Applying migrations..."
-python manage.py migrate --noinput
-
-# ------------------------------
 # Collect static files
 # ------------------------------
 echo "Collecting static files..."
